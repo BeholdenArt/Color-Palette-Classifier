5a1dbda4c2cb57a08727df4608c623b2c8035eeb
BeholdenArt <priyanshub5645@gmail.com>
2022-07-11 15:01:15 +0530

    Performance improvement

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
+from configparser import Interpolation
+from turtle import color
 import streamlit as st 
 from collections import Counter 
 from sklearn.cluster import KMeans
 import addons
 import cv2
+from sklearn import metrics 
 
 st.set_page_config("Color Palette Finder", "Assets/site logo.png")
 st.header(
@@ -25,7 +28,7 @@ st.sidebar.title("Enter Your Attributes Here...")
 
 uploaded_file = addons.uploading_image()
 k_value = st.sidebar.slider("Palette Size", min_value=5, max_value= 50, value= 25, step= 5)
-value_choice = st.sidebar.radio("Type of represent the colors", ["Hex Values", "Color Name"])
+color_choice = st.sidebar.radio("Type of represent the colors", ["Hex Values", "Color Name"])
 button = st.sidebar.button("Find Color Palette", False)
 
 if uploaded_file is None and button:
@@ -36,12 +39,7 @@ try:
         with st.spinner("Processing..."):
             raw_image = addons.config_image(uploaded_file)
             img = raw_image
-
-            if raw_image.shape[:2] >= (900, 600):
-                img = cv2.resize(raw_image, (900, 600), interpolation= cv2.INTER_AREA)
-            else:
-                img = cv2.resize(raw_image, (900, 600), interpolation= cv2.INTER_CUBIC)
-
+            img = cv2.resize(raw_image, (8, 8), interpolation= cv2.INTER_AREA)
             img = img.reshape(img.shape[0] * img.shape[1], 3)
 
 
@@ -55,10 +53,10 @@ try:
             
             st.image(raw_image)
 
-            if value_choice == "Hex Values":
+            if color_choice == "Hex Values":
                 fig1 = addons.draw_piechart(counts, hex_code, hex_code)
-            
-            if value_choice == "Color Name":
+    
+            if color_choice == "Color Name":
                 fig1 = addons.draw_piechart(counts, color_name, hex_code)
 
             st.pyplot(fig1)
